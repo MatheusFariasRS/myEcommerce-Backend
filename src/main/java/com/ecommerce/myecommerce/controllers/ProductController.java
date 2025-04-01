336a109979120771d600347b60c9970db8e2c9df
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-01 11:26:57 -0300

    Criando DTO e estruturando camadas

--- src/main/java/com/ecommerce/myecommerce/controllers/ProductController.java
+++ src/main/java/com/ecommerce/myecommerce/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.ecommerce.myecommerce.controllers;
 
+import com.ecommerce.myecommerce.dto.ProductDTO;
 import com.ecommerce.myecommerce.entities.Product;
 import com.ecommerce.myecommerce.repositories.ProductRepository;
+import com.ecommerce.myecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +17,13 @@ import java.util.Optional;
 public class ProductController {
 
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
 
-    @GetMapping
-    public String test(){
-       Optional<Product> result = repository.findById(1L);
-       Product product = result.get(); //como pegar o produto dentro do optional
-        return product.getName();
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        /*
+        ProductDTO dto = service.findById(id);
+        return dto;*/
+        return service.findById(id);
     }
 }
---
+++ src/main/java/com/ecommerce/myecommerce/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.ecommerce.myecommerce.services;
+
+import com.ecommerce.myecommerce.dto.ProductDTO;
+import com.ecommerce.myecommerce.entities.Product;
+import com.ecommerce.myecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+        /*
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        ProductDTO dto = new ProductDTO(product);
+        return dto;
+        */
+         Product product = repository.findById(id).get();
+         return new ProductDTO(product);
+    }
+}
