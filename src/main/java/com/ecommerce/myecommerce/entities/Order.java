17e13ca12635700cff21f3581e7b8f82d9e06755
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-01 09:23:18 -0300

    Primeiro teste da API - camada Controllers - ProductController

--- src/main/java/com/ecommerce/myecommerce/entities/Order.java
+++ src/main/java/com/ecommerce/myecommerce/entities/Order.java
@@ -21,7 +21,7 @@ public class Order {
     private OrderStatus status;
 
     @ManyToOne
-    @JoinColumn(name = "client _id")
+    @JoinColumn(name = "client_id")
     private User client;
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
