fffa661d0828571ce481c4d4e2e039a891151a6e
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-04 04:42:01 -0300

    Exception handling

--- src/main/java/com/ecommerce/myecommerce/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/ecommerce/myecommerce/services/exceptions/ResourceNotFoundException.java
@@ -2,6 +2,7 @@ package com.ecommerce.myecommerce.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+
     public ResourceNotFoundException(String msg){
         super(msg);
     }
