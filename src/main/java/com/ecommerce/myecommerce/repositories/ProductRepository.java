e634b15869cf26bf2a4f1f2b8e4646ccc4f0b3d6
Matheus Farias <matheusaf.contato@gmail.com>
2025-05-12 11:01:21 -0300

    consulta de produtos por nome

--- src/main/java/com/ecommerce/myecommerce/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/myecommerce/repositories/ProductRepository.java
@@ -1,7 +1,16 @@
 package com.ecommerce.myecommerce.repositories;
 
 import com.ecommerce.myecommerce.entities.Product;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj " +
+            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
 }
