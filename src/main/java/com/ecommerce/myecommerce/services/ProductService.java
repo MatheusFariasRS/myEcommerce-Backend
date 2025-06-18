d98494af2c073475b479b309ec9bb38224398000
Matheus Farias <matheusaf.contato@gmail.com>
2025-06-18 16:19:25 -0300

    ProductMinDTO

--- src/main/java/com/ecommerce/myecommerce/services/ProductService.java
+++ src/main/java/com/ecommerce/myecommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.myecommerce.services;
 
 import com.ecommerce.myecommerce.dto.ProductDTO;
+import com.ecommerce.myecommerce.dto.ProductMinDTO;
 import com.ecommerce.myecommerce.entities.Product;
 import com.ecommerce.myecommerce.repositories.ProductRepository;
 import com.ecommerce.myecommerce.services.exceptions.DatabaseException;
@@ -34,9 +35,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable){
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable){
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
