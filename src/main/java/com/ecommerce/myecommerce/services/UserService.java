98c7ce09a8aba56592198924a514605cc4a0df44
Matheus Farias <matheusaf.contato@gmail.com>
2025-06-18 16:12:21 -0300

    Obtendo o usuário logado

--- src/main/java/com/ecommerce/myecommerce/services/UserService.java
+++ src/main/java/com/ecommerce/myecommerce/services/UserService.java
@@ -1,14 +1,19 @@
 package com.ecommerce.myecommerce.services;
 
+import com.ecommerce.myecommerce.dto.UserDTO;
 import com.ecommerce.myecommerce.entities.Role;
 import com.ecommerce.myecommerce.entities.User;
 import com.ecommerce.myecommerce.projection.UserDetailsProjection;
 import com.ecommerce.myecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,5 +38,24 @@ public class UserService implements UserDetailsService {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
         return user;
+    }
+
+    protected User authenticated(){
+        try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            return repository.findByEmail(username).get();
+        }
+        catch (Exception e){
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe(){
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
